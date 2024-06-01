09cd0981eee906e1f3fa84d79bbfea64c7872b15
Alessandro <professornpc@gmail.com>
2024-06-01 18:05:09 -0300

    É necessário anotar a classe ProdutoController com @RestController

--- src/main/java/br/com/applip1/controller/ProdutoController.java
+++ src/main/java/br/com/applip1/controller/ProdutoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.applip1.cloneprof.model.Produto;
 
+@RestController
 public class ProdutoController {
 
 	private static List<Produto> listaProdutos;
